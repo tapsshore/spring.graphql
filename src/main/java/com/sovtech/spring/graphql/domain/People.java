1c1291cff9c7cbc72c8743cc5af6b847e4ed3228
Tapiwanashe Shoshore <tapiwanashe.shoshore@wonderlabz.com>
2022-08-14 16:18:38 +0200

    create People entity

---
+++ src/main/java/com/sovtech/spring/graphql/domain/People.java
@@ -0,0 +1,13 @@
+package com.sovtech.spring.graphql.domain;
+
+import lombok.Data;
+
+@Data
+public class People {
+
+  String name;
+  String height;
+  String mass;
+  String gender;
+  String homeworld;
+}
